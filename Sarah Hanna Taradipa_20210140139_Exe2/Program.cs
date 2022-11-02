989965345add415dfbfc327d93f5c78e90e880f1
sarahannata <sarahannata@gmail.com>
2022-11-02 15:42:00 +0700

    Membuat class Program

--- Sarah Hanna Taradipa_20210140139_Exe2/Program.cs
+++ Sarah Hanna Taradipa_20210140139_Exe2/Program.cs
@@ -1,1 +1,14 @@
-﻿
+﻿using System;
+
+namespace Exe2
+{
+    class Program
+    {
+        //Array to be searched
+        int[] arr = new int[32];
+        //Number of elements in the array
+        int n;
+        //Get the number of elements to store in the array
+        int i;
+    }
+}
